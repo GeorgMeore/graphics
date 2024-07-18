c61016fe1f56d30e99ba004dbb099bab8e461cf4
ggp <>
2024-07-19 01:20:03 +0300

    make gcc shut the fuck up when using this macro on an unsigned type

--- imath.h
+++ imath.h
@@ -1,4 +1,4 @@
-#define SIGN(a) ((a > 0) - (a < 0))
+#define SIGN(a) (((a) > 0) + ((a) == 0) - ((a) <= 0))
 #define ABS(a) ((a)*SIGN(a))
 #define DIVCEIL(n, d) ((ABS(n) + ABS(d) - 1) / d * SIGN(n))
 #define DIVROUND(n, d) ((ABS(n) + ABS(d)/2) / d * SIGN(n))
