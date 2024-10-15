01e0f7ea90faaaba860af47667985286fc96118f
ggp <georgii.galushko@gmail.com>
2024-10-15 10:05:26 +0300

    minor fix

--- fmt.h
+++ fmt.h
@@ -2,9 +2,9 @@
 
 #define FMTEND (U)0, 0 /* End of arguments, isn't supposed to be used explicitly */
 
-#define OD(v) (U)0, _INTFMT(typeof(v)), 10, (U64)v
-#define OH(v) (U)0, _INTFMT(typeof(v)), 16, (U64)v
-#define OB(v) (U)0, _INTFMT(typeof(v)), 2,  (U64)v
+#define OD(v) (U)0, _INTFMT(typeof(v)), 10, (U64)(v)
+#define OH(v) (U)0, _INTFMT(typeof(v)), 16, (U64)(v)
+#define OB(v) (U)0, _INTFMT(typeof(v)), 2,  (U64)(v)
 
 void _fdprint(int fd, ...);
 
@@ -13,7 +13,7 @@ void _fdprint(int fd, ...);
 #define eprint(...) _fdprint(2, __VA_ARGS__, FMTEND)
 #define eprintln(...) eprint(__VA_ARGS__, "\n")
 
-#define ID(v) (U)0, _INTFMT(typeof(*v)), (U)v
+#define ID(v) (U)0, _INTFMT(typeof(*(v))), (U)(v)
 
 int _fdinputln(int fd, ...);
 
