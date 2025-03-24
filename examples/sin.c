1c70007da81985973ac824bcbf7fd1121536370e
ggp <georgii.galushko@gmail.com>
2025-03-24 20:08:49 +0300

    fix time scaling

--- examples/sin.c
+++ examples/sin.c
@@ -13,13 +13,13 @@ int main(int, char **argv)
 	F64 t = 0;
 	while (!keyisdown('q')) {
 		Image *fb = framebegin();
-		t += lastframetime()/1e10;
+		t += lastframetime()/1e9;
 		drawclear(fb, BGCOLOR);
 		for (int i = 0; i < 200; i++) {
-			drawsmoothcircle(fb, fb->w*i/200, fb->h/2 + fsin(t*5)*fsin(t*5 + 4*PI*i/200)*fb->h/2, 5, RGBA(110, 70, 70, 255));
-			drawsmoothcircle(fb, fb->w*i/200, fb->h/2 + fcos(t*5)*fcos(t*4 + 4*PI*i/200)*fb->h/2, 5, RGBA(70, 110, 70, 255));
-			drawsmoothcircle(fb, fb->w*i/200, fb->h/2 + fsin(t*5)*fsin(t*3 + 4*PI*i/200 + PI)*fb->h/2, 5, RGBA(70, 70, 110, 255));
-			drawsmoothcircle(fb, fb->w*i/200, fb->h/2 + fcos(t*5)*fsin(t*2 + 4*PI*i/200 + 3*PI/2)*fb->h/2, 5, RGBA(110, 110, 110, 255));
+			drawsmoothcircle(fb, fb->w*i/200, fb->h/2 + fsin(t)*fsin(t + 4*PI*i/200)*fb->h/2, 5, RGBA(110, 70, 70, 255));
+			drawsmoothcircle(fb, fb->w*i/200, fb->h/2 + fcos(t)*fcos(t*.8 + 4*PI*i/200)*fb->h/2, 5, RGBA(70, 110, 70, 255));
+			drawsmoothcircle(fb, fb->w*i/200, fb->h/2 + fsin(t)*fsin(t*.6 + 4*PI*i/200 + PI)*fb->h/2, 5, RGBA(70, 70, 110, 255));
+			drawsmoothcircle(fb, fb->w*i/200, fb->h/2 + fcos(t)*fsin(t*.4 + 4*PI*i/200 + 3*PI/2)*fb->h/2, 5, RGBA(110, 110, 110, 255));
 		}
 		frameend();
 	}
