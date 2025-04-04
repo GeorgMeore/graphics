92d7c68d435390a2be5f078b7c290b93a895e85b
ggp <georgii.galushko@gmail.com>
2025-04-04 09:20:26 +0300

    avx and some 3d stuff, just experimenting

--- examples/line.c
+++ examples/line.c
@@ -32,7 +32,7 @@ int main(void)
 		drawclear(fb, BLACK);
 		for (int i = 0; i < 2; i++)
 			drawsmoothcircle(fb, pt[i][0], pt[i][1], 5, c[i]);
-		drawline(fb, pt[0][0], pt[0][1], pt[1][0], pt[1][1], WHITE);
+		drawthickline(fb, pt[0][0], pt[0][1], pt[1][0], pt[1][1], 10, RGBA(100, 100, 100, 100));
 		frameend();
 	}
 	winclose();
