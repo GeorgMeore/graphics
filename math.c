19303cb4de57323424ae36f531ea8015bb6c9ef7
ggp <georgii.galushko@gmail.com>
2025-04-18 21:40:27 +0300

    implemented a cubic root function

--- math.c
+++ math.c
@@ -59,6 +59,31 @@ F64 fsqrt(F64 x)
 	return a;
 }
 
+/* TODO: binary search is computationally more predictable but
+ * is slower than Newton's method, I should research that */
+F64 fcbrt(F64 x)
+{
+	if (x == 0)
+		return x;
+	_F64 _x = {x};
+	if (_x.exp == 0b11111111111)
+		return _x.v;
+	if (_x.sign)
+		x = -x;
+	F64 l = 0, r = x > 1 ? x : 1;
+	for (F64 lp = l-1, rp = r+1; l > lp || r < rp;) {
+		F64 m = l + (r - l)/2, y = m*m*m - x;
+		lp = l, rp = r;
+		if (y <= 0)
+			l = m;
+		if (y >= 0)
+			r = m;
+	}
+	if (_x.sign)
+		l = -l;
+	return l;
+}
+
 /* TODO: tailor series are not good for sin/cos, but it'll do for now */
 F64 fsin(F64 x)
 {
--- math.h
+++ math.h
@@ -3,6 +3,7 @@
 U64 isqrt(U64 x);
 
 F64 fsqrt(F64 x);
+F64 fcbrt(F64 x);
 F64 ffloor(F64 x);
 F64 fsin(F64);
 F64 fcos(F64);
