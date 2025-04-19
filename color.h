30056f9e517e42eccc9004b5ada6ffa10712732b
ggp <georgii.galushko@gmail.com>
2025-04-20 00:30:31 +0300

    predefine more colors

--- color.h
+++ color.h
@@ -11,12 +11,14 @@ typedef U32 Color;
 #define G(c) (((c)&0x0000FF00)>>(8*1))
 #define B(c) (((c)&0x000000FF)>>(8*0))
 
-#define GREEN  RGBA(0, 255, 0, 255)
-#define RED    RGBA(255, 0, 0, 255)
-#define BLUE   RGBA(0, 0, 255, 255)
-#define BLACK  RGBA(0, 0, 0, 255)
-#define WHITE  RGBA(255, 255, 255, 255)
-#define YELLOW RGBA(255, 255, 000, 255)
+#define GREEN   RGBA(0, 255, 0, 255)
+#define RED     RGBA(255, 0, 0, 255)
+#define BLUE    RGBA(0, 0, 255, 255)
+#define CYAN    RGBA(0, 255, 255, 255)
+#define MAGENTA RGBA(255, 0, 255, 255)
+#define YELLOW  RGBA(255, 255, 000, 255)
+#define BLACK   RGBA(0, 0, 0, 255)
+#define WHITE   RGBA(255, 255, 255, 255)
 
 Color blend(Color b, Color t);
 Color compose(Color b, Color t);
