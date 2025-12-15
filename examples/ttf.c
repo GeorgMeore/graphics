34fcd198cafabe55b9e37e3ca1dc266e253c2a64
ggp <georgii.galushko@gmail.com>
2025-12-15 07:34:59 +0300

    transform ttf glyphs into a sane format

--- examples/ttf.c
+++ examples/ttf.c
@@ -33,15 +33,22 @@ static U32 strtag(char s[4])
 	return s[3]<<(0*8)|s[2]<<(1*8)|s[1]<<(2*8)|s[0]<<(3*8);
 }
 
+typedef enum {
+	SegLine,
+	SegQuad,
+} Segtype;
+
 typedef struct {
-	I16 ncont;
-	U16 *ends;
-	U16 nvert;
-	I16 *xy[2];
-	I16 lim[2][2];
-	U8  *on;
-	U16 advance;
-	I16 lsb;
+	I16 c[3][2];
+	U8  type;
+} Segment;
+
+typedef struct {
+	I16     nseg;
+	Segment *segs;
+	I16     lim[2][2];
+	U16     advance;
+	I16     lsb;
 } Glyph;
 
 typedef struct {
@@ -57,36 +64,41 @@ typedef struct {
 	U16   *ctable[2];
 } Font;
 
-/* NOTE: When starting to parse a glyph we don't know how many points we'll have
- * to restore, but we can be sure that after the restoration the number of points
- * can at most double. We also know the max number of points a glyph can have.
- *
- * So we can do the following:
- *  1) allocate enough memory to hold twice the max number of points
- *  2) read all the points from the font file into the "upper" half
- *  3) move the points into the "lower" half, while restoring missing points */
-static OK restorepts(Glyph *g, U16 maxpts)
+typedef struct {
+	I16 ncont;
+	U16 *ends;
+	U16 nvert;
+	I16 *xy[2];
+	U8  *on;
+} Points;
+
+static void pointstoglyph(Glyph *g, Points *p)
 {
-	for (I16 cont = 0, start = maxpts, j = 0; cont < g->ncont; cont++) {
-		U16 n = g->ends[cont] + 1 - start;
-		I16 first = j;
-		for (U16 i = 0; i < n; i++, j++) {
-			U16 curr = start + i, prev = start + MOD(i-1, n);
-			I16 x1 = g->xy[0][curr], y1 = g->xy[1][curr];
-			if (!g->on[curr] && !g->on[prev]) {
-				I16 x2 = g->xy[0][prev], y2 = g->xy[1][prev];
-				g->xy[0][j] = (x1+x2)/2, g->xy[1][j] = (y1+y2)/2, g->on[j] = 1;
-				j++;
+	U16 nseg = 0;
+	for (I16 cont = 0, start = 0; cont < p->ncont; cont++) {
+		U16 n = p->ends[cont] + 1 - start;
+		for (U16 i = 0; i < n; i++) {
+			U16 curr = start + i;
+			U16 prev = start + MOD(i - 1, n);
+			U16 next = start + MOD(i + 1, n);
+			I16 x1 = p->xy[0][prev], y1 = p->xy[1][prev];
+			I16 x2 = p->xy[0][curr], y2 = p->xy[1][curr];
+			I16 x3 = p->xy[0][next], y3 = p->xy[1][next];
+			if (!p->on[curr]) {
+				if (!p->on[prev])
+					x1 = (x1 + x2)/2, y1 = (y1 + y2)/2;
+				if (!p->on[next])
+					x3 = (x3 + x2)/2, y3 = (y3 + y2)/2;
+				g->segs[nseg] = (Segment){{{x1, y1}, {x2, y2}, {x3, y3}}, SegQuad};
+				nseg += 1;
+			} else if (p->on[next]) {
+				g->segs[nseg] = (Segment){{{x2, y2}, {x3, y3}}, SegLine};
+				nseg += 1;
 			}
-			g->xy[0][j] = x1, g->xy[1][j] = y1, g->on[j] = g->on[curr];
 		}
-		if (!g->on[first])
-			return 0; /* TODO: handle first off curve */
-		g->ends[cont] = j-1;
 		start += n;
-		g->nvert = j;
 	}
-	return 1;
+	g->nseg = nseg;
 }
 
 typedef enum {
@@ -98,53 +110,53 @@ typedef enum {
 	YFlag   = 1<<5,
 } SimpFlag;
 
-static OK parsesimpleglyph(IOBuffer *b, Glyph *g, I16 ncont, U16 maxconts, U16 maxpts)
+static OK parsesimpleglyph(IOBuffer *b, Points *p, I16 ncont, U16 maxconts, U16 maxpts)
 {
-	if (g->ncont + ncont > maxconts)
+	if (p->ncont + ncont > maxconts)
 		return 0;
 	U16 nvert = 0;
 	for (I16 i = 0; i < ncont; i++) {
 		U16 idx = readbe(b, 2);
-		g->ends[g->ncont+i] = g->nvert + idx;
+		p->ends[p->ncont+i] = p->nvert + idx;
 		if (nvert > idx)
 			return 0;
 		nvert = idx + 1;
 	}
-	if (g->nvert + nvert > 2*maxpts)
+	if (p->nvert + nvert > maxpts)
 		return 0;
 	U16 ilen = readbe(b, 2);
 	skip(b, ilen); /* instructions */
 	for (I16 i = 0; i < nvert; i++) {
-		g->on[g->nvert+i] = readbe(b, 1);
-		if (g->on[g->nvert+i] & Repeat) {
+		p->on[p->nvert+i] = readbe(b, 1);
+		if (p->on[p->nvert+i] & Repeat) {
 			U8 count = readbe(b, 1);
 			if (count >= nvert - i)
 				return 0;
 			for (; count; count--, i++)
-				g->on[g->nvert+i+1] = g->on[g->nvert+i];
+				p->on[p->nvert+i+1] = p->on[p->nvert+i];
 		}
 	}
 	U8 cflags[2][2] = {{XShort, XFlag}, {YShort, YFlag}};
 	for (I16 comp = 0; comp < 2; comp++) {
 		for (I16 i = 0, prev = 0; i < nvert; i++) {
-			if (g->on[g->nvert+i] & cflags[comp][0]) {
-				if (g->on[g->nvert+i] & cflags[comp][1])
-					g->xy[comp][g->nvert+i] = prev + readbe(b, 1);
+			if (p->on[p->nvert+i] & cflags[comp][0]) {
+				if (p->on[p->nvert+i] & cflags[comp][1])
+					p->xy[comp][p->nvert+i] = prev + readbe(b, 1);
 				else
-					g->xy[comp][g->nvert+i] = prev - readbe(b, 1);
+					p->xy[comp][p->nvert+i] = prev - readbe(b, 1);
 			} else {
-				if (g->on[g->nvert+i] & cflags[comp][1])
-					g->xy[comp][g->nvert+i] = prev;
+				if (p->on[p->nvert+i] & cflags[comp][1])
+					p->xy[comp][p->nvert+i] = prev;
 				else
-					g->xy[comp][g->nvert+i] = prev + readbe(b, 2);
+					p->xy[comp][p->nvert+i] = prev + readbe(b, 2);
 			}
-			prev = g->xy[comp][g->nvert+i];
+			prev = p->xy[comp][p->nvert+i];
 		}
 	}
 	for (I16 i = 0; i < nvert; i++)
-		g->on[g->nvert+i] &= OnCurve;
-	g->nvert += nvert;
-	g->ncont += ncont;
+		p->on[p->nvert+i] &= OnCurve;
+	p->nvert += nvert;
+	p->ncont += ncont;
 	return 1;
 }
 
@@ -157,7 +169,7 @@ typedef enum {
 	Have2x2      = 1<<7,
 } CompFlag;
 
-static OK parsecompoundglyph(IOBuffer *b, Glyph *g, U32 glyf, U32 *locations, U16 maxconts, U16 maxpts)
+static OK parsecompoundglyph(IOBuffer *b, Points *p, U32 glyf, U32 *locations, U16 maxconts, U16 maxpts)
 {
 	for (;;) {
 		U16 flags = readbe(b, 2);
@@ -175,7 +187,7 @@ static OK parsecompoundglyph(IOBuffer *b, Glyph *g, U32 glyf, U32 *locations, U1
 			y = (I8)readbe(b, 1);
 		}
 		U64 curr = b->pos;
-		U16 start = g->nvert;
+		U16 start = p->nvert;
 		if (locations[index] == locations[index+1])
 			continue;
 		bseek(b, glyf + locations[index]);
@@ -183,14 +195,14 @@ static OK parsecompoundglyph(IOBuffer *b, Glyph *g, U32 glyf, U32 *locations, U1
 		skip(b, 2+2+2+2); /* xMin, yMin, xMax, yMax */
 		OK ok;
 		if (ncont > 0)
-			ok = parsesimpleglyph(b, g, ncont, maxconts, maxpts);
+			ok = parsesimpleglyph(b, p, ncont, maxconts, maxpts);
 		if (ncont < 0)
-			ok = parsecompoundglyph(b, g, glyf, locations, maxconts, maxpts);
+			ok = parsecompoundglyph(b, p, glyf, locations, maxconts, maxpts);
 		if (!ok)
 			return 0;
-		for (U16 i = start; i < g->nvert; i++) {
-			g->xy[0][i] += x;
-			g->xy[1][i] += y;
+		for (U16 i = start; i < p->nvert; i++) {
+			p->xy[0][i] += x;
+			p->xy[1][i] += y;
 		}
 		bseek(b, curr);
 		if (~flags & MoreComp)
@@ -199,9 +211,9 @@ static OK parsecompoundglyph(IOBuffer *b, Glyph *g, U32 glyf, U32 *locations, U1
 	return 1;
 }
 
-static OK parseglyph(IOBuffer *b, Font *f, U16 index, U32 glyf, U32 *locations, U16 maxconts, U16 maxpts)
+static OK parseglyph(IOBuffer *b, Font *f, Points *p, U16 index, U32 glyf, U32 *locations, U16 maxconts, U16 maxpts)
 {
-	if (locations[index] == locations[index+1])
+	if (index+1 < f->nglyph && locations[index] == locations[index+1])
 		return 1;
 	bseek(b, glyf + locations[index]);
 	I16 ncont = readbe(b, 2);
@@ -219,23 +231,18 @@ static OK parseglyph(IOBuffer *b, Font *f, U16 index, U32 glyf, U32 *locations, 
 		g->lim[1][0] >= f->lim[1][0] && g->lim[1][1] <= f->lim[1][1];
 	if (!ok)
 		return 0;
-	g->nvert = maxpts;
-	g->on = aralloc(&f->mem, maxpts*2);
-	g->ends = aralloc(&f->mem, maxconts * sizeof(U16));
-	g->xy[0] = aralloc(&f->mem, maxpts*2 * sizeof(I16));
-	g->xy[1] = aralloc(&f->mem, maxpts*2 * sizeof(I16));
+	p->nvert = p->ncont = 0;
 	if (ncont > 0)
-		ok = parsesimpleglyph(b, g, ncont, maxconts, maxpts);
+		ok = parsesimpleglyph(b, p, ncont, maxconts, maxpts);
 	else
-		ok = parsecompoundglyph(b, g, glyf, locations, maxconts, maxpts);
+		ok = parsecompoundglyph(b, p, glyf, locations, maxconts, maxpts);
 	if (!ok)
 		return 0;
 	for (I16 comp = 0; comp < 2; comp++)
-	for (U16 i = maxpts; i < g->nvert; i++)
-		if (g->xy[comp][i] < g->lim[comp][0] || g->xy[comp][i] > g->lim[comp][1])
+	for (U16 i = 0; i < p->nvert; i++)
+		if (p->xy[comp][i] < g->lim[comp][0] || p->xy[comp][i] > g->lim[comp][1])
 			return 0;
-	if (!restorepts(g, maxpts))
-		return 0;
+	pointstoglyph(g, p);
 	return 1;
 }
 
@@ -271,9 +278,17 @@ static OK parseglyphs(IOBuffer *b, Font *f, U32 head, U32 maxp, U32 glyf, U32 lo
 		locations[i] = offset*locascale;
 	}
 	f->glyphs = aralloc(&f->mem, f->nglyph * sizeof(Glyph));
+	Points tmp = {
+		.on    = aralloc(&f->mem, maxpts),
+		.ends  = aralloc(&f->mem, maxconts * sizeof(U16)),
+		.xy[0] = aralloc(&f->mem, maxpts * sizeof(I16)),
+		.xy[1] = aralloc(&f->mem, maxpts * sizeof(I16)),
+	};
+	/* a contour of n points consists of no more than n+1 segments */
+	U16 maxsegs = maxpts + maxconts;
 	for (U16 i = 0; i < f->nglyph; i++) {
-		f->glyphs[i] = (Glyph){0};
-		parseglyph(b, f, i, glyf, locations, maxconts, maxpts);
+		f->glyphs[i].segs = aralloc(&f->mem, maxsegs * sizeof(Segment));
+		parseglyph(b, f, &tmp, i, glyf, locations, maxconts, maxpts);
 	}
 	return 1;
 }
@@ -513,7 +528,7 @@ static I32 isectcurve(I16 rx, I16 ry, I16 x1, I16 y1, I16 x2, I16 y2, I16 x3, I1
 
 void drawraster(Image *f, I16 x0, I16 y0, Glyph g, Color c, F64 scale, U8 ss)
 {
-	if (!g.ncont)
+	if (!g.nseg)
 		return;
 	I16 xmin = g.lim[0][0]*scale - 1, xmax = g.lim[0][1]*scale + 1;
 	I16 ymin = g.lim[1][0]*scale - 1, ymax = g.lim[1][1]*scale + 1;
@@ -523,22 +538,15 @@ void drawraster(Image *f, I16 x0, I16 y0, Glyph g, Color c, F64 scale, U8 ss)
 		for (I16 dx = 0; dx < ss; dx++)
 		for (I16 dy = 0; dy < ss; dy++) {
 			I32 wn = 0;
-			for (I16 cont = 0, start = 0; cont < g.ncont; cont++) {
-				U16 n = g.ends[cont]+1-start;
-				for (U16 i = 0; i < n;) {
-					U16 curr = start + i, next = start + MOD(i+1, n), last = start + MOD(i+2, n);
-					I16 x1 = g.xy[0][curr]*ss*scale, y1 = g.xy[1][curr]*ss*scale;
-					I16 x2 = g.xy[0][next]*ss*scale, y2 = g.xy[1][next]*ss*scale;
-					I16 x3 = g.xy[0][last]*ss*scale, y3 = g.xy[1][last]*ss*scale;
-					if (g.on[next]) {
-						wn += isectline((x-x0)*ss + dx, (y0-y)*ss + dy, x1, y1, x2, y2);
-						i += 1;
-					} else {
-						wn += isectcurve((x-x0)*ss + dx, (y0-y)*ss + dy, x1, y1, x2, y2, x3, y3);
-						i += 2;
-					}
-				}
-				start += n;
+			for (I i = 0; i < g.nseg; i++) {
+				Segment s = g.segs[i];
+				I16 x1 = s.c[0][0]*ss*scale, y1 = s.c[0][1]*ss*scale;
+				I16 x2 = s.c[1][0]*ss*scale, y2 = s.c[1][1]*ss*scale;
+				I16 x3 = s.c[2][0]*ss*scale, y3 = s.c[2][1]*ss*scale;
+				if (s.type == SegQuad)
+					wn += isectcurve((x-x0)*ss + dx, (y0-y)*ss + dy, x1, y1, x2, y2, x3, y3);
+				else
+					wn += isectline((x-x0)*ss + dx, (y0-y)*ss + dy, x1, y1, x2, y2);
 			}
 			hits += wn != 0;
 		}
@@ -550,7 +558,7 @@ void drawraster(Image *f, I16 x0, I16 y0, Glyph g, Color c, F64 scale, U8 ss)
 /* TODO: more stuff for SDF */
 void drawsdf(Image *f, I16 x0, I16 y0, Glyph g, F64 scale)
 {
-	if (!g.ncont)
+	if (!g.nseg)
 		return;
 	I16 xmin = g.lim[0][0]*scale - 1, xmax = g.lim[0][1]*scale + 1;
 	I16 ymin = g.lim[1][0]*scale - 1, ymax = g.lim[1][1]*scale + 1;
@@ -558,31 +566,25 @@ void drawsdf(Image *f, I16 x0, I16 y0, Glyph g, F64 scale)
 	for (I16 y = CLIPY(f, y0 - ymax); y < CLIPY(f, y0 - ymin + 1); y++) {
 		I32 wn = 0;
 		F64 d = INF;
-		for (I16 cont = 0, start = 0; cont < g.ncont; cont++) {
-			U16 n = g.ends[cont]+1-start;
-			for (U16 i = 0; i < n;) {
-				U16 curr = start + i, next = start + MOD(i+1, n), last = start + MOD(i+2, n);
-				I16 x1 = g.xy[0][curr]*scale, y1 = g.xy[1][curr]*scale;
-				I16 x2 = g.xy[0][next]*scale, y2 = g.xy[1][next]*scale;
-				I16 x3 = g.xy[0][last]*scale, y3 = g.xy[1][last]*scale;
-				if (g.on[next]) {
-					wn += isectline(x - x0, y0 - y, x1, y1, x2, y2);
-					i += 1;
-					x3 = x2, y3 = y2;
-				} else {
-					wn += isectcurve(x - x0, y0 - y, x1, y1, x2, y2, x3, y3);
-					i += 2;
-				}
-				Poly dx = {{(x1 - x + x0), 2*(x2 - x1), x3 + x1 - 2*x2}, 2};
-				Poly dy = {{(y1 - y0 + y), 2*(y2 - y1), y3 + y1 - 2*y2}, 2};
-				Poly d2 = padd(pmul(dx, dx), pmul(dy, dy));
-				Roots r = roots(ddx(d2));
-				d = MIN3(d, eval(d2, 0), eval(d2, 1));
-				for (U8 i = 0; i < r.n; i++)
-					if (r.v[i] > 0 && r.v[i] < 1)
-						d = MIN(d, eval(d2, r.v[i]));
+		for (I i = 0; i < g.nseg; i++) {
+			Segment s = g.segs[i];
+			I16 x1 = s.c[0][0]*scale, y1 = s.c[0][1]*scale;
+			I16 x2 = s.c[1][0]*scale, y2 = s.c[1][1]*scale;
+			I16 x3 = s.c[2][0]*scale, y3 = s.c[2][1]*scale;
+			if (s.type == SegQuad) {
+				wn += isectcurve(x - x0, y0 - y, x1, y1, x2, y2, x3, y3);
+			} else {
+				wn += isectline(x - x0, y0 - y, x1, y1, x2, y2);
+				x3 = x2, y3 = y2;
 			}
-			start += n;
+			Poly dx = {{(x1 - x + x0), 2*(x2 - x1), x3 + x1 - 2*x2}, 2};
+			Poly dy = {{(y1 - y0 + y), 2*(y2 - y1), y3 + y1 - 2*y2}, 2};
+			Poly d2 = padd(pmul(dx, dx), pmul(dy, dy));
+			Roots r = roots(ddx(d2));
+			d = MIN3(d, eval(d2, 0), eval(d2, 1));
+			for (U8 i = 0; i < r.n; i++)
+				if (r.v[i] > 0 && r.v[i] < 1)
+					d = MIN(d, eval(d2, r.v[i]));
 		}
 		*(F32 *)&PIXEL(f, x, y) = fsetsign(d, wn);
 	}
@@ -674,43 +676,37 @@ static Isects isectcurve2(I16 ry, I16 x1, I16 y1, I16 x2, I16 y2, I16 x3, I16 y3
 /* TODO: implement a version wiht aa */
 void drawraster2(Image *f, I16 x0, I16 y0, Glyph g, Color c, F64 scale)
 {
-	if (!g.ncont)
+	if (!g.nseg)
 		return;
 	I16 xmin = g.lim[0][0]*scale - 1, xmax = g.lim[0][1]*scale + 1;
 	I16 ymin = g.lim[1][0]*scale - 1, ymax = g.lim[1][1]*scale + 1;
 	for (I16 y = CLIPY(f, y0 - ymax); y < CLIPY(f, y0 - ymin + 1); y++)
 	for (I16 x = CLIPX(f, x0 + xmin); x < CLIPX(f, x0 + xmax + 1); x++)
 		PIXEL(f, x, y) = 0;
-	for (I16 cont = 0, start = 0; cont < g.ncont; cont++) {
-		U16 n = g.ends[cont]+1-start;
-		for (U16 i = 0; i < n;) {
-			U16 curr = start + i, next = start + MOD(i+1, n), last = start + MOD(i+2, n);
-			I16 x1 = g.xy[0][curr]*scale, y1 = g.xy[1][curr]*scale;
-			I16 x2 = g.xy[0][next]*scale, y2 = g.xy[1][next]*scale;
-			I16 x3 = g.xy[0][last]*scale, y3 = g.xy[1][last]*scale;
-			if (g.on[next]) {
-				I16 ylo = MIN(y1, y2), yhi = MAX(y1, y2);
-				for (I16 y = CLIPY(f, y0 - yhi); y < CLIPY(f, y0 - ylo + 1); y++) {
-					Isects is = isectline2(y0 - y, x1, y1, x2, y2);
-					I16 x = CLIPX(f, x0 + is.x[0] + 1) - 1;
+	for (I i = 0; i < g.nseg; i++) {
+		Segment s = g.segs[i];
+		I16 x1 = s.c[0][0]*scale, y1 = s.c[0][1]*scale;
+		I16 x2 = s.c[1][0]*scale, y2 = s.c[1][1]*scale;
+		I16 x3 = s.c[2][0]*scale, y3 = s.c[2][1]*scale;
+		if (s.type == SegQuad) {
+			I16 ylo = MIN3(y1, y2, y3), yhi = MAX3(y1, y2, y3);
+			for (I16 y = CLIPY(f, y0 - yhi); y < CLIPY(f, y0 - ylo + 1); y++) {
+				Isects is = isectcurve2(y0 - y, x1, y1, x2, y2, x3, y3);
+				for (I16 j = 0; j < is.n; j++) {
+					I16 x = CLIPX(f, x0 + is.x[j] + 1) - 1;
 					if (x >= 0)
-						PIXEL(f, x, y) += is.wn[0];
+						PIXEL(f, x, y) += is.wn[j];
 				}
-				i += 1;
-			} else {
-				I16 ylo = MIN3(y1, y2, y3), yhi = MAX3(y1, y2, y3);
-				for (I16 y = CLIPY(f, y0 - yhi); y < CLIPY(f, y0 - ylo + 1); y++) {
-					Isects is = isectcurve2(y0 - y, x1, y1, x2, y2, x3, y3);
-					for (I16 j = 0; j < is.n; j++) {
-						I16 x = CLIPX(f, x0 + is.x[j] + 1) - 1;
-						if (x >= 0)
-							PIXEL(f, x, y) += is.wn[j];
-					}
-				}
-				i += 2;
+			}
+		} else {
+			I16 ylo = MIN(y1, y2), yhi = MAX(y1, y2);
+			for (I16 y = CLIPY(f, y0 - yhi); y < CLIPY(f, y0 - ylo + 1); y++) {
+				Isects is = isectline2(y0 - y, x1, y1, x2, y2);
+				I16 x = CLIPX(f, x0 + is.x[0] + 1) - 1;
+				if (x >= 0)
+					PIXEL(f, x, y) += is.wn[0];
 			}
 		}
-		start += n;
 	}
 	for (I16 y = CLIPY(f, y0 - ymax); y < CLIPY(f, y0 - ymin + 1); y++) {
 		I32 wn = 0;
@@ -772,7 +768,7 @@ F64 distcurve(F64 x, F64 y, F64 x1, F64 y1, F64 x2, F64 y2, F64 x3, F64 y3, F64 
 
 void drawsdf2(Image *f, I16 x0, I16 y0, Glyph g, F64 scale)
 {
-	if (!g.ncont)
+	if (!g.nseg)
 		return;
 	drawraster2(f, x0, y0, g, 1, scale); /* calculate signs */
 	I16 xmin = g.lim[0][0]*scale - 1, xmax = g.lim[0][1]*scale + 1;
@@ -780,22 +776,15 @@ void drawsdf2(Image *f, I16 x0, I16 y0, Glyph g, F64 scale)
 	for (I16 x = CLIPX(f, x0 + xmin); x < CLIPX(f, x0 + xmax + 1); x++)
 	for (I16 y = CLIPY(f, y0 - ymax); y < CLIPY(f, y0 - ymin + 1); y++) {
 		F64 d = INF;
-		for (I16 cont = 0, start = 0; cont < g.ncont; cont++) {
-			U16 n = g.ends[cont]+1-start;
-			for (U16 i = 0; i < n;) {
-				U16 curr = start + i, next = start + MOD(i+1, n), last = start + MOD(i+2, n);
-				I16 x1 = g.xy[0][curr]*scale, y1 = g.xy[1][curr]*scale;
-				I16 x2 = g.xy[0][next]*scale, y2 = g.xy[1][next]*scale;
-				I16 x3 = g.xy[0][last]*scale, y3 = g.xy[1][last]*scale;
-				if (g.on[next]) {
-					d = MIN(d, distline(x - x0, y0 - y, x1, y1, x2, y2));
-					i += 1;
-				} else {
-					d = MIN(d, distcurve(x - x0, y0 - y, x1, y1, x2, y2, x3, y3, d));
-					i += 2;
-				}
-			}
-			start += n;
+		for (I i = 0; i < g.nseg; i++) {
+			Segment s = g.segs[i];
+			I16 x1 = s.c[0][0]*scale, y1 = s.c[0][1]*scale;
+			I16 x2 = s.c[1][0]*scale, y2 = s.c[1][1]*scale;
+			I16 x3 = s.c[2][0]*scale, y3 = s.c[2][1]*scale;
+			if (s.type == SegQuad)
+				d = MIN(d, distcurve(x - x0, y0 - y, x1, y1, x2, y2, x3, y3, d));
+			else
+				d = MIN(d, distline(x - x0, y0 - y, x1, y1, x2, y2));
 		}
 		*(F32 *)&PIXEL(f, x, y) = fsetsign(d, PIXEL(f, x, y));
 	}
@@ -804,31 +793,21 @@ void drawsdf2(Image *f, I16 x0, I16 y0, Glyph g, F64 scale)
 	for (I16 y = CLIPY(f, y0 - ymax); y < CLIPY(f, y0 - ymin + 1); y++) {
 		F32 d = *(F32 *)&PIXEL(f, x, y);
 		F32 a = smoothstep(5, 0, ABS(d)) * 255;
-		if (d > 0)
-			PIXEL(f, x, y) = RGBA(a, a, 0, 255);
-		else
-			PIXEL(f, x, y) = RGBA(a, 0, a, 255);
+		PIXEL(f, x, y) = RGBA(a, a, a, 255);
 	}
 }
 
 void drawoutline(Image *f, I16 x0, I16 y0, Glyph g, Color c, F64 scale)
 {
-	for (I16 cont = 0, start = 0; cont < g.ncont; cont++) {
-		U16 n = g.ends[cont]+1-start;
-		for (U16 i = 0; i < n;) {
-			U16 curr = start + i, next = start + MOD(i+1, n), last = start + MOD(i+2, n);
-			I16 x1 = g.xy[0][curr]*scale, y1 = g.xy[1][curr]*scale;
-			I16 x2 = g.xy[0][next]*scale, y2 = g.xy[1][next]*scale;
-			I16 x3 = g.xy[0][last]*scale, y3 = g.xy[1][last]*scale;
-			if (g.on[next]) {
-				drawline(f, x0+x1, y0-y1, x0+x2, y0-y2, c);
-				i += 1;
-			} else {
-				drawbezier(f, x0+x1, y0-y1, x0+x2, y0-y2, x0+x3, y0-y3, c);
-				i += 2;
-			}
-		}
-		start += n;
+	for (I i = 0; i < g.nseg; i++) {
+		Segment s = g.segs[i];
+		I16 x1 = s.c[0][0]*scale, y1 = s.c[0][1]*scale;
+		I16 x2 = s.c[1][0]*scale, y2 = s.c[1][1]*scale;
+		I16 x3 = s.c[2][0]*scale, y3 = s.c[2][1]*scale;
+		if (s.type == SegQuad)
+			drawbezier(f, x0+x1, y0-y1, x0+x2, y0-y2, x0+x3, y0-y3, c);
+		else
+			drawline(f, x0+x1, y0-y1, x0+x2, y0-y2, c);
 	}
 }
 
